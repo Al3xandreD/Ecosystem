1cb79cb7ab72459c1332353ffa7d8aac162ab1a3
RedSkyHeaven <64643529+RedSkyHeaven@users.noreply.github.com>
2024-01-02 19:31:45 +0100

    finally added Fly and score...

--- src/main/java/org/example/Fly.java
+++ src/main/java/org/example/Fly.java
@@ -6,11 +6,23 @@ public class Fly extends Animal{
     static private int maxLife=3;
     private int f_speed;    // flying speed
 
-    public Fly(float pos_x , float pos_y, int f_speed){
-        super(maxLife, pos_x, pos_y);
-        this.f_speed=f_speed;
+    public int getAge() {
+        return age;
     }
 
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    private int age;
+    public Fly(float pos_x , float pos_y, int f_speed,int age){
+        super(maxLife, pos_x, pos_y);
+        this.f_speed=f_speed;
+        this.age=age;
+    }
+    public Fly(float pos_x , float pos_y, int f_speed){
+        this(pos_x , pos_y, f_speed,1);
+    }
     public int getF_speed() {
         return f_speed;
     }
